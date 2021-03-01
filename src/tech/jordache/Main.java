5ca8d9b0738478347e06a785303164a5ff7f50cc
Stefan Iordache <s.iordache@gmail.com>
2021-03-01 23:36:11 +0200

    Commit on test

--- src/tech/jordache/Main.java
+++ src/tech/jordache/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(("Hello World!"));
+        for( int i=0;i<99;i++){
+            System.out.println("Vai de capul meu");
+        }
     }
 }
